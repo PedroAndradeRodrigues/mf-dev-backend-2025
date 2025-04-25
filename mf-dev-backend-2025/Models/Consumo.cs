266c153b74680d17ca63801c0e96a08af6139fbc
PedroAndradeRodrigues <pedro.andrade.rodrigues@gmail.com>
2025-04-25 15:25:47 -0300

    Definição da classe "Consumo" e criação de tabela no banco de dados

---
+++ mf-dev-backend-2025/Models/Consumo.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace mf_dev_backend_2025.Models
+{
+
+    [Table("Consumos")]
+    public class Consumo
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório informar a descrição")]
+        [Display(Name ="Descrição")]
+        public string Descricao { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório informar a data")]
+        public DateTime Data { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório informar o valor")]
+        public decimal valor { get; set; }
+
+
+        [Required(ErrorMessage = "Obrigatório informar a quilometragem")]
+        public int Km { get; set; }
+
+        [Display(Name = "Tipo de Combustível")]
+        public TipoCombustivel Tipo { get; set; }
+
+        [Display(Name = "Veículo")]
+        public int VeiculoId { get; set; }
+
+        [ForeignKey("VeiculoId")]
+
+        public Veiculo Veiculo { get; set; }
+
+    }
+
+    public enum TipoCombustivel
+    { 
+        Gasolina,
+        Etanol
+    }
+}
